3fcae334fd8160fe324738a741d61b18108b2990
Karina Amador <akarina.at@gmail.com>
2018-05-23 18:36:25 -0700

    RENDERING IMAGES

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const PORT = process.env.PORT;
 
 const client = new pg.Client( process.env.DATABASE_URL );
 client.connect();
-client.on( 'error', err => console.error( error ) );
+client.on( 'error', err => console.error( err ) );
 
 app.use( cors() );
-app.get( '/', ( req, res ) => res.send( 'Testing 1, 2, 3' ) );
-app.get( '*', ( req, res ) => res.status( 403 ).send( 'This route does not exist.' ) );
-app.listen( PORT, () => console.log( `Listening on port: ${PORT}` ) );
+app.use(express.json());
+app.use(express.urlencoded({extend:true}));
+app.get( '/', ( req, res ) => res.send( 'Testing 1, 2, 3' ));
+
+
+app.get('/api/v1/books', (request, response) => {
+  console.log(request.body);
+  client.query(`SELECT * FROM books;`)
+  .then(result => response.send(result.rows))
+  .catch(console.error);
+})
+
+app.get( '*', ( req, res ) => res.status( 403 ).send( 'This route does not exist.' ));
+
+app.listen( PORT, () => console.log( `Listening on port: ${PORT}` ));
+
